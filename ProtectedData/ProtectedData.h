6cbb3baea30d615c51784b3d2f9d2e066a010622
Gongcu <knmy0101@naver.com>
2024-01-26 23:30:22 +0900

    Initial Commit

---
+++ ProtectedData/ProtectedData.h
@@ -0,0 +1,18 @@
+//
+//  ProtectedData.h
+//  ProtectedData
+//
+//  Created by 공채운 on 1/26/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ProtectedData.
+FOUNDATION_EXPORT double ProtectedDataVersionNumber;
+
+//! Project version string for ProtectedData.
+FOUNDATION_EXPORT const unsigned char ProtectedDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ProtectedData/PublicHeader.h>
+
+
